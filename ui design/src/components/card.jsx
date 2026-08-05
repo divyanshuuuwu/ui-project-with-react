fac88bf864d5561dcbc049c6d57035b490e0a947
divyanshu <divyanshusah44@gmail.com>
2026-08-05 20:55:59 +0530

    bg fix

--- ui design/src/components/card.jsx
+++ ui design/src/components/card.jsx
@@ -3,7 +3,7 @@ import cardimg from '../assets/cardimg.jpg'
 
 const Card = () => {
   return (
-    <div className='bg-zinc-100 w-100 h-50 rounded-4xl flex px-5 gap-5 py-5 '>
+    <div className='bg-zinc-200 w-100 h-50 rounded-4xl flex px-5 gap-5 py-5 '>
         <div className='w-30 h-40 bg-zinc-500 flex shrink-0 rounded-4xl overflow-hidden '>
         <img src={cardimg} alt="" className='w-full h-full object-cover'/>
         </div>
