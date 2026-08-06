9e0303bfff0f8af70822f309cd5dbb5687fa81e4
divyanshu <divyanshusah44@gmail.com>
2026-08-06 23:51:04 +0530

    tailwind and axios installed

--- gallery project/src/App.jsx
+++ gallery project/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import axios from 'axios'
+
 
 const App = () => {
+
+const getData = async()=>{
+  const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=100")
+  console.log(response.data[0].author)
+}
+
+
   return (
-    <div>App</div>
+    <>
+
+<div className='background bg-black h-screen p-5 text-white'>
+  <button
+  onClick={getData} 
+  className='bg-green-500 py-3 px-5 rounded-4xl'>show data
+  </button>
+</div>
+
+    </>
   )
 }
 
